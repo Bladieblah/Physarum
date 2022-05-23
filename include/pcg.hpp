4ad4901112e6b6d200fe218ce00b1d30115dd2a1
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-05-23 11:15:50 +0200

    Got it pretty

---
+++ include/pcg.hpp
@@ -0,0 +1,124 @@
+/*
+ * PCG Random Number Generation for C.
+ *
+ * Copyright 2014 Melissa O'Neill <oneill@pcg-random.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For additional information about the PCG random number generation scheme,
+ * including its license and other licensing options, visit
+ *
+ *     http://www.pcg-random.org
+ */
+
+/*
+ * This code is derived from the full C implementation, which is in turn
+ * derived from the canonical C++ PCG implementation. The C++ version
+ * has many additional features and is preferable if you can use C++ in
+ * your project.
+ */
+
+#ifndef PCG_H_INCLUDED
+#define PCG_H_INCLUDED 1
+
+#include <inttypes.h>
+
+#define PCG_MAX (double)4294967295.0
+#define PCG_MAX1 (double)4294967296.0
+
+#if __cplusplus
+extern "C" {
+#endif
+
+/* Coefficients in rational approximations. */
+static const double a[] =
+{
+	-3.969683028665376e+01,
+	 2.209460984245205e+02,
+	-2.759285104469687e+02,
+	 1.383577518672690e+02,
+	-3.066479806614716e+01,
+	 2.506628277459239e+00
+};
+
+static const double b[] =
+{
+	-5.447609879822406e+01,
+	 1.615858368580409e+02,
+	-1.556989798598866e+02,
+	 6.680131188771972e+01,
+	-1.328068155288572e+01
+};
+
+static const double c[] =
+{
+	-7.784894002430293e-03,
+	-3.223964580411365e-01,
+	-2.400758277161838e+00,
+	-2.549732539343734e+00,
+	 4.374664141464968e+00,
+	 2.938163982698783e+00
+};
+
+static const double d[] =
+{
+	7.784695709041462e-03,
+	3.224671290700398e-01,
+	2.445134137142996e+00,
+	3.754408661907416e+00
+};
+
+struct pcg_state_setseq_64 {    // Internals are *Private*.
+    uint64_t state;             // RNG state.  All values are possible.
+    uint64_t inc;               // Controls which RNG sequence (stream) is
+                                // selected. Must *always* be odd.
+};
+typedef struct pcg_state_setseq_64 pcg32_random_t;
+
+// If you *must* statically initialize it, here's one.
+
+#define PCG32_INITIALIZER   { 0x853c49e6748fea9bULL, 0xda3e39cb94b95bdbULL }
+
+// pcg32_srandom(initstate, initseq)
+// pcg32_srandom_r(rng, initstate, initseq):
+//     Seed the rng.  Specified in two parts, state initializer and a
+//     sequence selection constant (a.k.a. stream id)
+
+void pcg32_srandom(uint64_t initstate, uint64_t initseq);
+void pcg32_srandom_r(pcg32_random_t* rng, uint64_t initstate,
+                     uint64_t initseq);
+
+// pcg32_random()
+// pcg32_random_r(rng)
+//     Generate a uniformly distributed 32-bit random number
+
+uint32_t pcg32_random(void);
+uint32_t pcg32_random_r(pcg32_random_t* rng);
+
+// pcg32_boundedrand(bound):
+// pcg32_boundedrand_r(rng, bound):
+//     Generate a uniformly distributed number, r, where 0 <= r < bound
+
+uint32_t pcg32_boundedrand(uint32_t bound);
+uint32_t pcg32_boundedrand_r(pcg32_random_t* rng, uint32_t bound);
+
+// Uniform number between 0 and 1
+double UNI();
+double UNI_r(pcg32_random_t* rng);
+double RANDN();
+
+#if __cplusplus
+}
+#endif
+
+#endif // PCG_BASIC_H_INCLUDED
--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,31 @@
 #include <time.h>
 #include <math.h>
 #include <vector>
+#include <iostream>
 
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #include <GLUT/glut.h>
 
 #include "colour.hpp"
+#include "SimplexNoise.hpp"
+#include "pcg.hpp"
 
 // Window size
 // #define size_x 1920
 // #define size_y 1080
-#define size_x 1400
-#define size_y 801
+#define size_x 3024
+#define size_y 1964
+// #define size_x 1400
+// #define size_y 801
 
-bool showColorBar = true;
-double one_9 = 1. / 9.;
+double size_x_inv = 1. / size_x;
+double size_y_inv = 1. / size_y;
+
+bool showColorBar = false;
 
 // Array to be drawn
 unsigned int data[size_y*size_x*3];
-
-double diffKernel[3][3] = {
-    {1./9., 1./9., 1./9.},
-    {1./9., 1./9., 1./9.},
-    {1./9., 1./9., 1./9.}
-};
 
 float *colourMap;
 int nColours = 255;
@@ -37,7 +38,15 @@ typedef struct Particle {
     double phi;
 } Particle;
 
-int nParticles = 10000;
+int nParticles = 200000;
+double sensorAngle = 45. / 180. * M_PI;
+double sensorDist = 9;
+double rotationAngle = 45. / 180. * M_PI / 18.;
+double particleStepSize = 2;
+double depositAmount = 0.04;
+double stableAverage = 0.1;
+double decay = 1 - (nParticles * depositAmount) / (stableAverage * size_x * size_y);
+double one_9 = 1. / 9. * decay;
 
 Particle *particles;
 double **trail, **trailDummy;
@@ -96,17 +105,49 @@ void initData() {
     int yc = size_y / 2;
 
     int R = size_y / 4;
-    double r, w = 15;
+    double r, w = 5;
+
+    double seedx = UNI() * 20 - 10;
+    double seedy = UNI() * 20 - 10;
 
     for (i = 0; i < size_x; i++) {
         for (j = 0; j < size_y; j++) {
             r = sqrt(pow(i - xc, 2) + pow(j - yc, 2));
-            trail[i][j] = exp(-pow((r - R) / w, 2));
+            // trail[i][j] = fmin(1., exp(-pow((r - R) / w, 2)) + pow((1 + SimplexNoise::noise(i * size_y_inv * 60, j * size_y_inv * 60)) / 2, 6) / 2);
+            trail[i][j] = fmin(1., pow((1 + SimplexNoise::noise((i * size_y_inv + seedx) * 10, (j * size_y_inv + seedy) * 10)) / 2, 6) / 2);
         }
     }
 }
 
+double clip(double in, double lower, double upper) {
+    if (in < lower) {
+        return lower;
+    }
+    if (in > upper) {
+        return upper;
+    }
+
+    return in;
+}
+
+void initParticles() {
+    int i;
+    Particle particle;
+
+    for (i = 0; i < nParticles; i++) {
+        particle = particles[i];
+
+        particle.x = clip(0.5 + RANDN() * 0.11999, 0.3, 0.7) * size_x;
+        particle.y = clip(0.5 + RANDN() * 0.11999, 0.3, 0.7) * size_y;
+        particle.phi = 2 * M_PI * UNI();
+
+        particles[i] = particle;
+    }
+}
+
 void prepare() {
+    pcg32_srandom(time(NULL) ^ (intptr_t)&printf, (intptr_t)&nParticles); // Seed pcg
+
     particles = (Particle *)malloc(nParticles * sizeof(Particle));
 
     trail = (double **)malloc(size_x * sizeof(double *));
@@ -118,6 +159,7 @@ void prepare() {
     }
 
     initData();
+    initParticles();
 }
 
 void cleanup() {
@@ -131,6 +173,71 @@ void cleanup() {
     }
     free(trail);
     free(trailDummy);
+}
+
+void moveParticle(Particle *particle) {
+    double fl, fc, fr;
+    double flx, fly, fcx, fcy, frx, fry;
+
+    // if (UNI() < 1.5) {
+        flx = particle->x + cos(particle->phi - sensorAngle) * sensorDist;
+        fly = particle->y + sin(particle->phi - sensorAngle) * sensorDist;
+
+        fcx = particle->x + cos(particle->phi) * sensorDist;
+        fcy = particle->y + sin(particle->phi) * sensorDist;
+
+        frx = particle->x + cos(particle->phi + sensorAngle) * sensorDist;
+        fry = particle->y + sin(particle->phi + sensorAngle) * sensorDist;
+
+        fl = trail[(int)(flx + size_x) % size_x][(int)(fly + size_y) % size_y];
+        fc = trail[(int)(fcx + size_x) % size_x][(int)(fcy + size_y) % size_y];
+        fr = trail[(int)(frx + size_x) % size_x][(int)(fry + size_y) % size_y];
+
+        if (fc < fl && fc < fr) {
+            particle->phi += rotationAngle * (UNI() > 0.5 ? 1 : -1);
+        }
+        else if (fl > fc && fc > fr) {
+            particle->phi -= rotationAngle;
+        }
+        else if (fl < fc && fc < fr) {
+            particle->phi += rotationAngle;
+        }
+    // }
+    // else {
+    //     particle->phi += rotationAngle * (UNI() > 0.5 ? 1 : -1);
+    // }
+
+    particle->x += cos(particle->phi) * particleStepSize;
+    particle->y += sin(particle->phi) * particleStepSize;
+
+    if (particle->x < 0) {
+        particle->x += size_x;
+    }
+    else if (particle->x >= size_x) {
+        particle->x -= size_x;
+    }
+
+    if (particle->y < 0) {
+        particle->y += size_y;
+    }
+    else if (particle->y >= size_y) {
+        particle->y -= size_y;
+    }
+}
+
+void iterParticles() {
+    int i;
+
+    for (i = 0; i < nParticles; i++) {
+        // std::cout << "phi: " << particles[i].phi << " y: " << particles[i].y << " // ";
+        moveParticle(&(particles[i]));
+        // std::cout << "phi: " << particles[i].phi << " y: " << particles[i].y << std::endl;
+    }
+
+    for (i = 0; i < nParticles; i++) {
+        // Deposit
+        trail[(int)particles[i].x][(int)particles[i].y] += depositAmount;
+    }
 }
 
 void diffuse() {
@@ -161,8 +268,8 @@ void diffuse() {
 }
 
 void step() {
+    iterParticles();
     diffuse();
-    processTrail();
 }
 
 void display() {
@@ -196,6 +303,7 @@ void display() {
     
     std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
     step();
+    processTrail();
     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
     
     std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
@@ -214,6 +322,9 @@ void key_pressed(unsigned char key, int x, int y) {
         case 'r':
             initData();
             break;
+        case 'i':
+            initParticles();
+            break;
         case 'b':
             showColorBar = !showColorBar;
             break;
@@ -228,6 +339,8 @@ void key_pressed(unsigned char key, int x, int y) {
 }
 
 int main(int argc, char **argv) {
+    // std::cout << fmod(-1, size_x) << std::endl;
+    fprintf(stderr, "Decay = %.4f\n", decay);
     prepare();
     makeColourmap();
     
